2e93ddc0ce3a6dbd61f0b9544dee8019bb6e6419
Allison Betancourt <allisonbbetancourt@gmail.com>
2015-05-17 16:55:16 -0500

    Cart total logic solved, now to put it in the shopping cart service

--- js/controllers/cartCtrl.js
+++ js/controllers/cartCtrl.js
@@ -1,12 +1,12 @@
 angular.module('awesome-shop').controller('cartCtrl', ['$scope','shoppingCart', function($scope,shoppingCart){
   $scope.cart = shoppingCart;
-  console.log($scope.cart.items);
 
   $scope.cartTotal = function(){
     var tot = 0;
-    angular.forEach($scope.cart.items, function(){
-      tot += $scope.cart.items.price;
+    angular.forEach($scope.cart.items, function(val){
+      tot += val.price * val.quantity;
     });
-    console.log(tot);
+
+    return tot;
   }
 }]);
